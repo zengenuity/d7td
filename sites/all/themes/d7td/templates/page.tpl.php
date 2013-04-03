5880870db58f62306b8584e7d75245a15dc48def
Wayne Eaker <wayne@zengenuity.com>
2013-04-03 13:58:09 -0400

    Step 6: fix navigation

--- sites/all/themes/d7td/templates/page.tpl.php
+++ sites/all/themes/d7td/templates/page.tpl.php
@@ -108,10 +108,10 @@
     </div></div> <!-- /.section, /#header -->
 
     <?php if ($main_menu || $secondary_menu): ?>
-      <div id="navigation"><div class="section">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-      </div></div> <!-- /.section, /#navigation -->
+      <div id="navigation"><div class="section clearfix">
+        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline')), 'heading' => t('Secondary menu'))); ?>
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline')), 'heading' => t('Main menu'))); ?>
+         </div></div> <!-- /.section, /#navigation -->
     <?php endif; ?>
 
     <?php if ($breadcrumb): ?>
