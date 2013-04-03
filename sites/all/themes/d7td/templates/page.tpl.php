d29536ac00d8ce6ca89549cc4afc5fbe839044d7
Wayne Eaker <wayne@zengenuity.com>
2013-04-03 15:05:39 -0400

    Step 12: move messages box

--- sites/all/themes/d7td/templates/page.tpl.php
+++ sites/all/themes/d7td/templates/page.tpl.php
@@ -118,8 +118,6 @@
       <div id="breadcrumb"><?php print $breadcrumb; ?></div>
     <?php endif; ?>
 
-    <?php print $messages; ?>
-
     <div id="main-wrapper"><div id="main" class="clearfix">
 
       <?php if ($page['sidebar_first']): ?>
@@ -130,6 +128,9 @@
         
       <div id="content" class="column"><div class="section">
         <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+        
+        <?php print $messages; ?>
+        
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
